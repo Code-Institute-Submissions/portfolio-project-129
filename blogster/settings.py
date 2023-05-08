8815aacb06b3ffd1822703714834e120bd1daf8e
Humanslaughter <theodorewikstrom@hotmail.com>
2023-05-08 10:42:29 +0000

    Deployment commit

--- blogster/settings.py
+++ blogster/settings.py
@@ -33,7 +33,7 @@ DEBUG = False
 
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
-ALLOWED_HOSTS = ['pp4-blogster.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['pp4-blogster.herokuapp.com']
 
 
 # Application definition
